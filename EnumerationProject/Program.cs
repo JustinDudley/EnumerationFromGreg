d8502097012827a384cbac257b2bdc26b5f314d2
Greg Doud <gpdoud@gmail.com>
2019-08-30 12:19:52 -0400

    Final commit

--- EnumerationProject/Program.cs
+++ EnumerationProject/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 
 namespace EnumerationProject {
+
+    enum GradStatus { SummaCumLaude, MagnaCumLaude, CumLaude }
+
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            GradStatus gs = GradStatus.MagnaCumLaude;
+            var i = (int)gs;
+
+            if(gs == GradStatus.MagnaCumLaude) {
+
+            }
+
         }
     }
 }
